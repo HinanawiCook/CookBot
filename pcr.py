743101e3a07e928e4257b3122a366a9f45bd8677
HinanawiCook <1442104687@qq.com>
2022-10-23 23:35:01 +0800

    优化输出格式

--- pcr.py
+++ pcr.py
@@ -7,29 +7,42 @@ def pcr_pick_ten():
     three_star_list = os.listdir('E:/CookBot/resource/pcr/3star')
     two_star_list = os.listdir('E:/CookBot/resource/pcr/2star')
     one_star_list = os.listdir('E:/CookBot/resource/pcr/1star')
-    pick_list = list()
+    three_star_pick_list = list()
+    two_star_pick_list = list()
+    one_star_pick_list = list()
     p_normal = np.array([0.795, 0.18, 0.025])
     p_tenth = np.array([0.975, 0.025])
+    result = '三星:'
     for i in range(9):
         normal_pick = np.random.choice([1, 2, 3], p=p_normal.ravel())
         if normal_pick == 3:
             three_star_pick = random.choice(three_star_list)
-            pick_list.append('#' + three_star_pick + '#')
+            three_star_pick_list.append(three_star_pick)
         elif normal_pick == 2:
             two_star_pick = random.choice(two_star_list)
-            pick_list.append(two_star_pick)
-        elif normal_pick == 1:
+            two_star_pick_list.append(two_star_pick)
+        else:
             one_star_pick = random.choice(one_star_list)
-            pick_list.append(one_star_pick)
+            one_star_pick_list.append(one_star_pick)
     tenth_pick = np.random.choice([2, 3], p=p_tenth.ravel())
     if tenth_pick == 3:
         three_star_pick = random.choice(three_star_list)
-        pick_list.append('#' + three_star_pick + '#')
-    elif tenth_pick == 2:
+        three_star_pick_list.append('#' + three_star_pick + '#')
+    else:
         two_star_pick = random.choice(two_star_list)
-        pick_list.append(two_star_pick)
-        print('十连！')
-        print(pick_list)
+        two_star_pick_list.append(two_star_pick)
+    if len(three_star_pick_list) == 0:
+        result = result + '没抽到！'
+    else:
+        for i in range(len(three_star_pick_list)):
+            result = result + ' ' + three_star_pick_list[i]
+    result = result + '\n' + '二星:'
+    for i in range(len(two_star_pick_list)):
+        result = result + ' ' + two_star_pick_list[i]
+    result = result + '\n' + '一星:'
+    for i in range(len(one_star_pick_list)):
+        result = result + ' ' + one_star_pick_list[i]
+    print(result)
 
 
 s = '抽卡pcrup'
